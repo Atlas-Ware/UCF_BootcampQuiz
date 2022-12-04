2c7dd1c3d9c420e84dd9d61d35eb8ced603ffcdd
Ignacio Albistu <profiles.albistu@gmail.com>
2022-12-04 14:39:05 -0500

    completed all the code, HTML, CSS, JS

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# UCF_BootcampQuiz
+# UCF Bootcamp Quiz
 We need to create an online quiz generating our own HTML, CSS &amp; JS
--- assets/js/script.js
+++ assets/js/script.js
@@ -75,33 +75,24 @@ var questions = [
 ];
 
 // Starts the quiz by clicking button "start quiz"
-document.querySelector("#start-button").addEventListener("click", startQuiz);
+document.querySelector("#start-button").addEventListener("click", beginQuiz);
 
 // BEGIN Quiz Code
-function startQuiz() {
-// Hide any visible cards, show the question card
+function beginQuiz() {
   hideCards();
   questionCard.removeAttribute("hidden");
-
-// Assign 0 to currentQuestion when start button is clicked, then display the current question on the page
   currentQuestion = 0;
-  displayQuestion();
-
-// Set total time depending on number of questions
+  showTheQuestion();
   time = questions.length * 10;
-
-// Executes function "countdown" every 1000ms to update time and display on page
   intervalID = setInterval(countdown, 1000);
-
-// Invoke displayTime here to ensure time appears on the page as soon as the start button is clicked, not after 1 second
-  displayTime();
+  showTime();
 }
 
-// If answer is incorrect, penalize time
-function checkAnswer(eventObject) {
+// Answer is wrong, you loose time
+function answer(eventObject) {
   var optionButton = eventObject.target;
   resultDiv.style.display = "block";
-  if (optionIsCorrect(optionButton)) {
+  if (optionCorrect(optionButton)) {
     resultText.textContent = "Correct!";
     setTimeout(hideResultText, 1000);
   } else {
@@ -109,27 +100,22 @@ function checkAnswer(eventObject) {
     setTimeout(hideResultText, 1000);
     if (time >= 10) {
       time = time - 10;
-      displayTime();
+      showTime();
     } else {
-// If time is less than 10, display time as 0 and end quiz
-// Time is set to zero in this case to avoid displaying a negative number in cases where a wrong answer is submitted with < 10 seconds left on the timer
       time = 0;
-      displayTime();
+      showTime();
       endQuiz();
     }
-  }
-
-// Increment current question by 1
-  currentQuestion++;
-  //if we have not run out of questions then display next question, else end quiz
+  } currentQuestion++;
   if (currentQuestion < questions.length) {
-    displayQuestion();
+    showTheQuestion();
   } else {
     endQuiz();
   }
 }
+
 // Display the question and answer options for the current question
-function displayQuestion() {
+function showTheQuestion() {
   var question = questions[currentQuestion];
   var options = question.options;
 
@@ -146,46 +132,43 @@ function displayQuestion() {
 
 
 // BEGIN Timer Code
-// Reduce time by 1 and display new value, if time runs out then end quiz
 function countdown() {
   time--;
-  displayTime();
+  showTime();
   if (time < 1) {
     endQuiz();
   }
 }
-
 // Display time on page
 var timeDisplay = document.querySelector("#time");
-function displayTime() {
+function showTime() {
   timeDisplay.textContent = time;
 }
 // END Timer Code
+
 
 // BEGIN Results Code
 var resultDiv = document.querySelector("#result-div");
 var resultText = document.querySelector("#result-text");
 
-// Hide result div
+// Hide result
 function hideResultText() {
   resultDiv.style.display = "none";
 }
 
-// Behaviour when an answer button is clicked: click event bubbles up to div with id "quiz-options"
-// EventObject.target identifies the specific button element that was clicked on
-document.querySelector("#quiz-options").addEventListener("click", checkAnswer);
+// Answer button is clicked
+document.querySelector("#quiz-options").addEventListener("click", answer);
 
-// Compare the text content of the option button with the answer to the current question
-function optionIsCorrect(optionButton) {
+
+function optionCorrect(optionButton) {
   return optionButton.textContent === questions[currentQuestion].answer;
 }
 // END Results Code
 
+
 // BEGIN Score Code
-// Display scorecard and hide other divs
 var score = document.querySelector("#score");
 
-// At end of quiz, clear the timer, hide any visible cards and 
 // display the scorecard and display the score as the remaining time
 function endQuiz() {
   clearInterval(intervalID);
@@ -197,46 +180,38 @@ function endQuiz() {
 var submitButton = document.querySelector("#submit-button");
 var inputElement = document.querySelector("#initials");
 
-//store user initials and score when submit button is clicked
+// Stores your initials and score when submit button is clicked
 submitButton.addEventListener("click", storeScore);
 
 function storeScore(event) {
-  //prevent default behaviour of form submission
   event.preventDefault();
-
-  //check for input
   if (!inputElement.value) {
     alert("Please enter your initials before pressing submit!");
     return;
   }
-
-  //store score and initials in an object
   let leaderboardItem = {
     initials: inputElement.value,
     score: time,
   };
 
-  updateStoredLeaderboard(leaderboardItem);
+  updateLeaderboard(leaderboardItem);
 
-  //hide the question card, display the leaderboardcard
+// Hides the question card & displays the leaderboardcard
   hideCards();
   leaderboardCard.removeAttribute("hidden");
-
-  renderLeaderboard();
+  displayLeaderboard();
 }
 
 // Updates the leaderboard stored in local storage
-function updateStoredLeaderboard(leaderboardItem) {
+function updateLeaderboard(leaderboardItem) {
   var leaderboardArray = getLeaderboard();
-  //append new leaderboard item to leaderboard array
   leaderboardArray.push(leaderboardItem);
   localStorage.setItem("leaderboardArray", JSON.stringify(leaderboardArray));
 }
 
-// ??Get "leaderboardArray" from local storage (if it exists) and parse 
-// it into a javascript object using JSON.parse??
+// Get "leaderboardArray" from local storage and parse 
+// it into a javascript object
 function getLeaderboard() {
-  //FROM HERE
   var storedLeaderboard = localStorage.getItem("leaderboardArray");
   if (storedLeaderboard !== null) {
     let leaderboardArray = JSON.parse(storedLeaderboard);
@@ -248,7 +223,7 @@ function getLeaderboard() {
 }
 
 // Display leaderboard on leaderboard card
-function renderLeaderboard() {
+function displayLeaderboard() {
   var sortedLeaderboardArray = sortLeaderboard();
   var highscoreList = document.querySelector("#highscore-list");
   highscoreList.innerHTML = "";
@@ -290,14 +265,13 @@ clearButton.addEventListener("click", clearHighscores);
 // Clear local storage and display empty leaderboard
 function clearHighscores() {
   localStorage.clear();
-  renderLeaderboard();
+  displayLeaderboard();
 }
 
 // RETURNs to Beginning
 // Back button & return to first card variable & event listener
 var backButton = document.querySelector("#back-button");
 backButton.addEventListener("click", returnToStart);
-
 
 
 // Use ID from HTML header to view highscores
@@ -307,14 +281,9 @@ leaderboardLink.addEventListener("click", showLeaderboard);
 function showLeaderboard() {
   hideCards();
   leaderboardCard.removeAttribute("hidden");
-
-  // Stop countdown
   clearInterval(intervalID);
-
-  // Assign undefined to time and display that way the time is hidden on the page
   time = undefined;
-  displayTime();
+  showTime();
 
-  // Displays leaderboard on leaderboard card
-  renderLeaderboard();
+  displayLeaderboard();
 }
